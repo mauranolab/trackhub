ef4bc46810c370dc9422c549c34fc70e17cf5e32
André M. Ribeiro-dos-Santos <andremrsantos@gmail.com>
2021-12-13 10:34:02 -0500

    Suppress shortLabel and longLabel warning (#4)
    Suppress shortLabel and long length warning since they don't produce any actual truncation to these fields
    
    Co-authored-by: mattmaurano <44711377+mattmaurano@users.noreply.github.com>

--- trackhub/validate.py
+++ trackhub/validate.py
@@ -267,20 +267,22 @@ def alphanumeric_(v):
 @validator('one two three', 'aaaaaaaaaaaaaaaaa')
 def short_label(v):
     assert isinstance(v, string_types)
-    if len(v) > 17:
-        warnings.warn(
-            "shortLabel is limited to 17 characters "
-            "in the browser, some characters will be truncated")
+    # suppressing warning (trackhub does not actually truncate, and neither does the browser)
+    # if len(v) > 17:
+    #     warnings.warn(
+    #         "shortLabel is limited to 17 characters "
+    #         "in the browser, some characters will be truncated")
     return True
 
 
 @validator('a' * 76, 'four five six')
 def long_label(v):
     assert isinstance(v, string_types)
-    if len(v) > 76:
-        warnings.warn(
-            "longLabel is limited to 76 characters "
-            "in the browser, some characters will be truncated")
+    # suppressing warning which appears inaccurate (trackhub does not actually truncate, and neither does the browser)
+    # if len(v) > 76:
+    #     warnings.warn(
+    #         "longLabel is limited to 76 characters "
+    #         "in the browser, some characters will be truncated")
     return True
 
 
